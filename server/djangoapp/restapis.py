54a6e4b0f2fd992f174c02d8c3ed279f12d3d06e
angeloDGomez <adgomez.dev@gmail.com>
2024-08-14 12:54:53 -0400

    Fixing code style

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -13,9 +13,9 @@ sentiment_analyzer_url = os.getenv(
 
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
+    if (kwargs):
         for key, value in kwargs.items():
-            params=params+key+"="+value+"&"
+            params = params + key + "=" + value + "&"
 
     request_url = backend_url+endpoint+"?"+params
 
@@ -24,7 +24,7 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except error:
         # If any error occurs
         print("Network exception occurred")
 
@@ -46,5 +46,5 @@ def post_review(data_dict):
         response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
+    except error:
         print("Network exception occurred")
